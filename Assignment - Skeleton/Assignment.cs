474cd8d99052a2e7638e07fd25be4b1588e38d3c
Trent Savage <trent.savage@aggiemail.usu.edu>
2022-01-31 21:52:17 -0700

    simple graphic displayed

--- Assignment - Skeleton/Assignment.cs
+++ Assignment - Skeleton/Assignment.cs
@@ -6,6 +6,11 @@ namespace CS5410
 {
     public class Assignment : Game
     {
+        Texture2D m_ballTexture;
+        Vector2 m_ballPosition;
+        float m_ballSpeed;
+
+
         private GraphicsDeviceManager m_graphics;
         private SpriteBatch m_spriteBatch;
 
@@ -19,6 +24,9 @@ namespace CS5410
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            m_ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            m_graphics.PreferredBackBufferHeight / 2);
+            m_ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -28,6 +36,12 @@ namespace CS5410
             m_spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            m_ballTexture = Content.Load<Texture2D>("ball");
+
+        }
+        protected void ProcessInput()
+        {
+            // does stuff. 
         }
 
         protected override void Update(GameTime gameTime)
@@ -47,6 +61,9 @@ namespace CS5410
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            m_spriteBatch.Begin();
+            m_spriteBatch.Draw(m_ballTexture, new Vector2(0, 0), Color.White);
+            m_spriteBatch.End();
 
             base.Draw(gameTime);
         }
