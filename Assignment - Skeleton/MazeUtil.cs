655bb94341224c2268333aa0c36906bf4180ab38
Trent Savage <trent.savage@aggiemail.usu.edu>
2022-02-12 20:05:21 -0700

    middle of implementing Prim's

--- Assignment - Skeleton/MazeUtil.cs
+++ Assignment - Skeleton/MazeUtil.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CS5410
@@ -9,7 +11,6 @@ namespace CS5410
     {
         public struct TileState
         {
-            
             public bool isVisited { get; set; }
             public bool isShortestPath { get; set; }
             public bool south { get; set; }
@@ -47,21 +48,62 @@ namespace CS5410
         /// <summary>
         /// Returns top-left node of maze. 
         /// </summary>
-        /// <param name="size">the length the maze can be along each side</param>
+        /// <param name="n">the length the maze can be along each side</param>
         /// <returns></returns>
-        private TileState[,] generateRandomMaze(int size)
+        private TileState[,] generateRandomMaze(int n)
         {
-            // demo: make a simple 2x2 maze with one wall in it. 
-
-            TileState[,] maze = new TileState[size, size];
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
+            TileState[,] maze = new TileState[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
                     maze[i, j] = new TileState();
 
             //  Prim's algorithm
+            var random = new Random();
+            var discovered = new List<(int,int)>();
+            var frontier = new List<(int,int)>();
 
+            (int, int) target = (random.Next(n - 1), random.Next(n - 1));
+
+            discovered.Add(target);
+            frontier = frontier.Union(getNeighbors(target, n)).ToList();
+
+            while (frontier.Count > 0)
+            {
+                target = frontier[random.Next(frontier.Count)];
+                // get neighbors of target in discovered and choose one randomly
+                List<(int, int)> candidatesList = getNeighbors(target,n).Intersect(discovered).ToList();
+                (int, int) targetee = candidatesList[random.Next(candidatesList.Count())];
+
+                // erase the wall between the randomly chosen discovered tile and the target
+                //(int, int) wallBetweenCoords = getWallCoords(target, targetee);
+
+                //pathMap[wallBetweenCoords.Item1, wallBetweenCoords.Item2] = true;
+
+                // establish path between the randomly chosen discovered tile and the target
+                
+
+                // put the target into discovered set
+                discovered.Add(target);
+                frontier.Remove(target);
+
+                // throw new neighbors into frontier
+                frontier = frontier.Union(getNeighbors(target,n).Except(candidatesList)).ToList();
+            }
 
             return maze;
+        }
+        List<(int, int)> getNeighbors((int,int) target, int n)
+        {
+            List<(int,int)> neighbors = new List<(int,int)>();
+            if (target.Item1 - 1 >= 0)
+                neighbors.Add((target.Item1 - 1, target.Item2));
+            if (target.Item1 + 1 < n)
+                neighbors.Add((target.Item1 + 1, target.Item2));
+            if (target.Item2 - 1 >= 0)
+                neighbors.Add((target.Item1, target.Item2 - 1));
+            if (target.Item2 + 1 < n)
+                neighbors.Add((target.Item1, target.Item2 + 1));
+            return neighbors;
         }
     }
 }
