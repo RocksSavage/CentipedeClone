b0eda27265dd3b939bcc2a579a9f54287320322e
Trent Savage <trent.savage@aggiemail.usu.edu>
2022-03-12 17:11:23 -0700

    ported select files to this branch, it SHOULDcompile still

---
+++ GameState - Menus/GameAgents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS5410
+{
+	public class GameAgents
+	{
+        public List<Objects.Shrooms> m_shroomsList = new List<Objects.Shrooms>();
+		//public List<Objects.Lazer> m_lazerList = new List<Objects.Lazer>();
+        public Objects.Player m_player;
+
+		public GameAgents()
+        {
+        }
+
+        public Objects.Shrooms shroomCollision(Objects.AnimatedSprite model)
+        {
+            foreach (Objects.Shrooms ee in m_shroomsList)
+            {
+                if (model.collide((Objects.AnimatedSprite)ee))
+                    return ee;
+            }
+            return null;
+        }
+        public void playerCollision(Objects.AnimatedSprite model)
+        {
+            //TODO
+        }
+    }
+}
---
+++ GameState - Menus/Objects/Lazer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CS5410.Objects
+{
+    public class Lazer : InanimateSprite
+    {
+        private float m_speed;
+        private GameAgents m_gameAgents;
+        public Lazer(Vector2 size, Vector2 center, GameAgents gameAgents, float speed) : base(size, center)
+        { 
+            m_speed = speed;
+            m_gameAgents = gameAgents;
+        }
+
+        public void moveUp(GameTime gameTime)
+        {
+            //TODO 
+            var nextspc = new Vector2(this.m_center.X,m_center.Y - m_speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            Shrooms collider = m_gameAgents.shroomCollision(new AnimatedSprite(this.Size, nextspc));
+            if (collider == null)
+                m_center.Y = nextspc.Y;
+        }
+    }
+}
